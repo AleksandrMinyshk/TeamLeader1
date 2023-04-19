f3613c74990c226956af49e7e7e634798cad1b59
AleksandrMinyshk <alexmech001@gmail.com>
2023-04-19 18:10:00 -0400

    memberA v.1

---
+++ src/test/java/Steps/pageInitializers.java
@@ -0,0 +1,13 @@
+package Steps;
+
+import Pages.LoginPage;
+
+public class pageInitializers {
+
+    public static LoginPage login;
+
+    public static void initializePageObjects(){
+        login=new LoginPage();
+
+    }
+}
